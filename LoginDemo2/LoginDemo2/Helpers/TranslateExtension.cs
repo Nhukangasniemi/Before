74b0bb15a78eee1019cadd09717e65a9527d6e49
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-01-27 12:48:27 +0200

    Add project files.

---
+++ LoginDemo2/LoginDemo2/Helpers/TranslateExtension.cs
@@ -0,0 +1,22 @@
+﻿using LoginDemo2.Resources;
+using System;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginDemo2.Helpers
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return null;
+
+            return AppResources.ResourceManager.GetString(Text, AppResources.Culture);
+        }
+    }
+}
